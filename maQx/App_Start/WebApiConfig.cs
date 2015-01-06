08ecdcd378968ddfaf28d997fe79117c1e3d3cd1
Prasanth <prashanth702@gmail.com>
2015-01-06 11:00:27 +0530

    Update WebApi Config

--- maQx/App_Start/WebApiConfig.cs
+++ maQx/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using maQx.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -13,24 +14,24 @@ namespace maQx
         {
             // Web API configuration and services
 
-            // Web API routes
+            // Web API routes           
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
 
             // Added OData Routing Support
 
-            //config.MapHttpAttributeRoutes();
-
-            //config.Routes.MapHttpRoute(
-            //    name: "DefaultApi",
-            //    routeTemplate: "api/{controller}/{id}",
-            //    defaults: new { id = RouteParameter.Optional }
-            //);
-
-            ODataModelBuilder builder = new ODataConventionModelBuilder();
-            builder.EntitySet<Plant>("Products");
-            config.MapODataServiceRoute(
-                routeName: "ODataRoute",
-                routePrefix: null,
-                model: builder.GetEdmModel());
+            //ODataModelBuilder builder = new ODataConventionModelBuilder();
+            //builder.EntitySet<JPlant>("Plants");
+            //config.MapODataServiceRoute(
+            //    routeName: "ODataRoute",
+            //    routePrefix: null,
+            //    model: builder.GetEdmModel());
         }
     }
 }
