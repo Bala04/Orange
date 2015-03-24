49c990df488e4d2cd53866ea97ced5f9b4bff5e6
Bala04 <bala.alab@live.com>
2015-03-24 13:33:58 +0530

    Wip&Signalr Connection

---
+++ maQx/Models/WorkInProgress.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using maQx.Models;
+
+namespace maQx.Models
+{
+    public static class WorkInProgress
+    {
+        public static void Calculate(ProductProcess ProductProcess, int Produced,int Rejected,WebApplication5Context db )
+        {            
+                Wip w = db.Wips.FirstOrDefault<Wip>(q => q.ProductProcess.ID == ProductProcess.ID);
+                if (w == null)
+                {
+                    w = new Wip();
+                    
+                    w.ProductProcess = ProductProcess;
+                    w.Input = Produced;
+                    w.Accepted = Produced - Rejected;
+                    w.Rejected = Rejected;
+                    w.UpdatedAt = DateTime.Now;
+                    db.Wips.Add(w);
+                    UpdateNextProcess(ProductProcess,w.Accepted,db);
+                }
+                else
+                {
+                    db.Entry(w).State = EntityState.Modified;
+                    w.ProductProcess = ProductProcess;
+                    if (Produced <= w.Input)
+                    {
+                        w.Input = w.Input -Produced;
+                        w.Accepted = Produced - Rejected;
+                        w.Rejected = Rejected;
+                        w.UpdatedAt = DateTime.Now;
+                        UpdateNextProcess(ProductProcess, w.Accepted,db);
+                    }
+                }
+            
+            
+        }
+        static void UpdateNextProcess(ProductProcess ProductProcess, int Accepted ,WebApplication5Context db)
+        {
+            ProductProcess NextProductProcess = db.ProductProcesses.FirstOrDefault(q => q.Product.Code == ProductProcess.Product.Code && q.Order == ProductProcess.Order + 1);
+
+            if (NextProductProcess != null)
+            {
+                //gets the next productprocess 
+                Wip nextw = db.Wips.FirstOrDefault<Wip>(e => e.ProductProcess.ID == NextProductProcess.ID);
+                if (nextw == null)
+                {
+                    nextw = new Wip();
+                    nextw.ProductProcess = NextProductProcess;
+                    nextw.Input = Accepted;
+                    nextw.UpdatedAt = DateTime.Now;
+                    db.Wips.Add(nextw);
+                }
+                else
+                {
+                    db.Entry(nextw).State = EntityState.Modified;
+                    nextw.UpdatedAt = DateTime.Now;
+                    //adds the previous accepter quantity
+                    nextw.Input = nextw.Input + Accepted;
+                }
+            }
+        }
+    }
+}
---
+++ maQx/Scripts/SignalR-Connection.js
@@ -0,0 +1,85 @@
+﻿$(function (mintimeout) {
+
+   function getData(c, b) {
+
+        var a = JSON.parse(c);
+
+        for (var i = 0; i < a.length; i++) {
+            if (a[i].actor == b) {
+                return a[i];
+            }
+        }
+
+        return "-1";
+    }
+    
+    function getServer(hub) {
+        return $.connection[hub].server;
+    }
+    function callBack(hub, name, func) {
+        $.connection[hub].client[name] = func;
+    }
+    function startServer(func) {
+        $.connection.hub.start().done(func);
+    }
+    function stopServer() {
+        $.connection.hub.stop();
+
+    }
+    function setOnStart(func) {
+        $.connection.hub.starting(func);
+    }
+    function setOnError(func) {
+        $.connection.hub.error(func);
+    }
+    var disconnect;
+    function setOnDisconnected(func) {
+        disconnect = func;
+    }
+    function setOnReconnected(func) {
+        $.connection.hub.reconnected(func);
+    }
+    function setOnConnectionSlow(func) {
+        $.connection.hub.connectionSlow(func);
+    }
+
+    var notify;
+    var d = false;
+   
+
+    $.connection.hub.logging = true;
+    
+    $.connection.hub.disconnected(function () {
+        console.log("disconnected");
+        d = true;
+        changeCount();
+        disconnect();
+    });
+
+    function reStartServer() {
+       
+        if (d) {
+            console.log("startServer");
+
+            $.connection.hub.start().done(
+                function () {
+                    d = false;
+                    count = mintimeout;
+                    setNewTimeout();
+                });
+
+        }
+    }
+    var count = mintimeout;
+    var interval = setTimeout(reStartServer, count);
+    function changeCount() {
+        console.log(count);
+        count = count * 2;
+        setNewTimeout();
+    }
+    function setNewTimeout() {
+        clearInterval(interval);
+        interval = setTimeout(reStartServer, count);
+    }
+
+});
